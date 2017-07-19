d3809bb263bc47b66cbef279362ff4d9ecd92bb2
ilhamefd <ilhamefd95@gmail.com>
2017-07-19 16:16:56 +0700

    halaman lapor

--- application/views/userpages/lapor/lapor.php
+++ application/views/userpages/lapor/lapor.php
@@ -1,11 +1,68 @@
 <div class="konten">
   <div class="container">
+  	<div class="container-fluid">
     <div class="row">
-    <h2>Lapor</h2>
+    <div class="col-md-4">
+    <center><h2>Lapor</h2></center>
     <hr>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br>
-   
+    <br>
+    
+    <div class="panel panel-primary">
+    	<div class="panel-heading">
+    	<br>
+	    <form class="form-horizontal">
+			<fieldset>
+				<!-- Text input-->
+				<div class="form-group">
+					<label class="col-md-4 control-label" for="nik">NIK</label>  
+				  		<div class="col-md-5">
+				  			<input id="nik" name="nik" type="text" placeholder="Masukkan NIK" class="form-control input-md" required="">
+				 		</div>
+				</div>
+
+				<!-- Text input-->
+				<div class="form-group">
+				  	<label class="col-md-4 control-label" for="nama">Nama</label>  
+				  		<div class="col-md-5">
+				  			<input id="nama" name="nama" type="text" placeholder="Masukkan nama anda" class="form-control input-md" required="">
+				      </div>
+				</div>
+
+				<!-- Text input-->
+				<div class="form-group">
+				  	<label class="col-md-4 control-label" for="alamat">alamat</label>  
+				  		<div class="col-md-5">
+				  			<input id="alamat" name="alamat" type="text" placeholder="Masukkan alamat anda" class="form-control input-md" required="">
+				    	</div>
+				</div>
+
+				<!-- Text input-->
+				<div class="form-group">
+				  	<label class="col-md-4 control-label" for="lokasi">Lokasi Kejadian</label>  
+				  		<div class="col-md-5">
+				  			<input id="lokasi" name="lokasi" type="text" placeholder="Masukkan lokasi kejadian" class="form-control input-md" required="">
+				    	</div>
+				</div>
+
+				<!-- File Button --> 
+				<div class="form-group">
+					<label class="col-md-4 control-label" for="foto">Foto</label>
+				  		<div class="col-md-4">
+				    		<input id="foto" name="foto" class="input-file" type="file">
+				  		</div>
+				</div>
+			</fieldset>
+		</form>
+		</div>
+		</div> 
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+	</div>
+	</div>
     </div>
   </div>
 </div>
