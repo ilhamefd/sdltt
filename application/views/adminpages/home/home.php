694d8fd6a8bc22182606c84fad65d4594433f687
ilhamefd <ilhamefd95@gmail.com>
2017-07-24 16:50:56 +0700

    data base

--- application/views/adminpages/home/home.php
+++ application/views/adminpages/home/home.php
@@ -4,7 +4,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">Home</h1>
-                        <hr>
+                       
 Selamat datang                    </div>
                     <!-- /.col-lg-12 -->
                 </div>
